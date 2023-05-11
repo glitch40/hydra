6302a904f447d243774d48fbba3ec333005e5874
Pascal Grange <pascal@grange.nom.fr>
2023-05-11 13:54:35 +0000

    WIP

--- nix/hydra/packages.nix
+++ nix/hydra/packages.nix
@@ -2,6 +2,8 @@
 
 { hydraProject # as defined in default.nix
 , system ? builtins.currentSystem
+, pkgs
+, cardano-node
 }:
 let
   nativePkgs = hydraProject.hsPkgs;
@@ -10,7 +12,7 @@ let
     ({ lib, ... }: { options.nonReinstallablePkgs = lib.mkOption { apply = lib.remove "terminfo"; }; });
   musl64Pkgs = patchedForCrossProject.projectCross.musl64.hsPkgs;
 in
-{
+rec {
   hydra-node = nativePkgs.hydra-node.components.exes.hydra-node;
   hydra-node-static = musl64Pkgs.hydra-node.components.exes.hydra-node;
   hydra-tools-static = musl64Pkgs.hydra-node.components.exes.hydra-tools;
@@ -18,4 +20,15 @@ in
   hydra-tui-static = musl64Pkgs.hydra-tui.components.exes.hydra-tui;
   hydraw = nativePkgs.hydraw.components.exes.hydraw;
   hydraw-static = musl64Pkgs.hydraw.components.exes.hydraw;
+  tests = {
+    hydra-node = nativePkgs.hydra-node.components.tests.tests;
+    hydra-cluster = pkgs.buildEnv {
+      name = "integration";
+      paths = [
+                nativePkgs.hydra-cluster.components.tests.integration
+                cardano-node.packages.${system}.cardano-node
+                hydra-node
+              ];
+    };
+  };
 }
